8b40d2d721a372e61ca66b0b5d110a727530f25f
j-mir-prazak <j.mir.prazak@gmail.com>
2019-06-02 23:40:10 +0200

    quick git

--- linaro/dhcp-ifup-setup.sh
+++ linaro/dhcp-ifup-setup.sh
@@ -4,33 +4,33 @@ path="/home/pi"
 
 if [[ ! -z $1 ]]; then echo $1; fi
 
-echo "" >> "$path"dhcp.status
-echo "-------------------------------------------------" >> "$path"dhcp.status
-echo "" >> "$path"dhcp.status
+echo "" >> "$path"/dhcp.status
+echo "-------------------------------------------------" >> "$path"/dhcp.status
+echo "" >> "$path"/dhcp.status
 
-echo "dhcp ifup startup" | tee -a "$path"dhcp.status
-date | tee -a "$path"dhcp.status
+echo "dhcp ifup startup" | tee -a "$path"/dhcp.status
+date | tee -a "$path"/dhcp.status
 
 if [ -f "/boot/dhcp-server" ]; then
 
-  sudo systemctl restart isc-dhcp-server | tee -a "$path"dhcp.status
-  echo "" >> "$path"dhcp.status
-  echo "-------------------------------------------------" >> "$path"dhcp.status
-  echo "" >> "$path"dhcp.status
-  #journalctl -xe >> "$path"dhcp.status
+  sudo systemctl restart isc-dhcp-server | tee -a "$path"/dhcp.status
+  echo "" >> "$path"/dhcp.status
+  echo "-------------------------------------------------" >> "$path"/dhcp.status
+  echo "" >> "$path"/dhcp.status
+  #journalctl -xe >> "$path"/dhcp.status
 
 elif [ -f "/boot/dhcp-client" ]; then
 
-  echo "" >> "$path"dhcp.status
-  echo "--------------------------------------------  -----" >> "$path"dhcp.status
-  echo "" >> "$path"dhcp.status
+  echo "" >> "$path"/dhcp.status
+  echo "--------------------------------------------  -----" >> "$path"/dhcp.status
+  echo "" >> "$path"/dhcp.status
 
 else
         echo "no dhcp setup specified"
 fi
 
-echo "" >> "$path"dhcp.status
-echo "-------------------------------------------------" >> "$path"dhcp.status
-echo "" >> "$path"dhcp.status
+echo "" >> "$path"/dhcp.status
+echo "-------------------------------------------------" >> "$path"/dhcp.status
+echo "" >> "$path"/dhcp.status
 
-ip a | tee -a "$path"dhcp.status
+ip a | tee -a "$path"/dhcp.status
