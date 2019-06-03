12c0f4236fbf46fd22232e1bd2394fb53943976f
j-mir-prazak <j.mir.prazak@gmail.com>
2019-06-03 02:44:24 +0200

    quick git

--- linaro/startup.sh
+++ linaro/startup.sh
@@ -2,15 +2,26 @@
 
 sudo chmod 0777 -R /home/*
 sudo chmod +x -R /etc/network/*
-
+c=0
 while true; do
-	sleep 30
+	c=c+1
+	sleep 1
+
 	  echo "-------------------------------------------------" >> "$log"
 		  echo "startup loop" >> "$log"
 
 	echo "-------------------------------------------------" >> "$log"
-	sudo systemctl restart isc-dhcp-server  | tee -a "$log"/dhcp.status
-  echo "" >> "$log"/dhcp.status
-  echo "-------------------------------------------------" >> "$log"
 
+	if systemctl is-active --quiet isc-dhcp-server.service; then
+		date >> "$log"/dhcp.status
+		echo "dhcp is running" >> "$log"/dhcp.status
+	else
+		echo "restarting dhcp" >> "$log"/dhcp.status
+		sudo systemctl restart isc-dhcp-server  | tee -a "$log"/dhcp.status
+	fi
+	echo "-------------------------------------------------" >> "$log"
+
+	if [ $c -eq 10]; then
+		echo "10 loops"
+	fi
 done
