dd32088ac710c0b05ceb95e524f372c01b809d44
j-mir-prazak <j.mir.prazak@gmail.com>
2019-06-03 23:42:41 +0200

    quick git

--- linaro/startup.sh
+++ linaro/startup.sh
@@ -70,6 +70,8 @@ elif [ -f "/boot/dhcp-client" ]; then
 				echo "restarting connection"
 				if fping -q -c4 -t1500 192.168.99.1 &>/dev/null; then
 					echo "saved by the master connection"
+				elif fping -q -c4 -t1500 8.8.8.8 &>/dev/null; then
+					echo "saved by the internet connection"
 				else
 					bash /home/pi/mk3/linaro/dhcp-startup-setup.sh
 				fi
