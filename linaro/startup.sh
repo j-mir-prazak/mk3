b304b4e85f9c992d7220c67508029f7d098b1e19
j-mir-prazak <j.mir.prazak@gmail.com>
2019-06-03 02:22:30 +0200

    quick git

--- linaro/startup.sh
+++ linaro/startup.sh
@@ -2,3 +2,15 @@
 
 sudo chmod 0777 -R /home/*
 sudo chmod +x -R /etc/network/*
+
+while true; do
+	sleep 30
+	  echo "-------------------------------------------------" >> "$log"
+		  echo "startup loop" >> "$log"
+
+	echo "-------------------------------------------------" >> "$log"
+	sudo systemctl restart isc-dhcp-server  | tee -a "$log"/dhcp.status
+  echo "" >> "$log"/dhcp.status
+  echo "-------------------------------------------------" >> "$log"
+
+done
